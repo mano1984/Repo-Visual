d6b08c61a5478abb7d58b7ab595448effd98bb9d
mano1984 <mn.fernandez@globant.com>
2013-11-22 15:38:59 -0200

    Primer commit

---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Chau Mundo", MsgBoxStyle.Information, "Chau")
+    End Sub
+End Class
